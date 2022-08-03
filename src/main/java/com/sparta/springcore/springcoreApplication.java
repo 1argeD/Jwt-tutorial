01941e5719118b0131165e8b29c00ca977bdf4cc
1argeD <94172354+1argeD@users.noreply.github.com>
2022-08-03 13:45:33 +0900

    나만의 셀랙샵 연습

---
+++ src/main/java/com/sparta/springcore/springcoreApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.springcore;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springcoreApplication {
+
+	public static void main(String[] args) {
+		springcoreApplication.run(springcoreApplication.class, args);
+	}
+
+}
